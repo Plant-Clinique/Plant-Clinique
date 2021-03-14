bfe7e11506b8c0d83a0bbef2075e814e7ec21e84
RomanEmpire314 <taromka21@brandeis.edu>
2021-03-14 17:34:11 -0400

    Set up Clearance before User inheritance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  include Clearance::User
+
     has_many :user_plants
     has_many :reminders
     has_many :chatbot_messages
     has_many :replies
     has_many :posts
+    validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_180612) do
+ActiveRecord::Schema.define(version: 2021_03_14_212606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,9 +63,16 @@ ActiveRecord::Schema.define(version: 2021_03_07_180612) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
