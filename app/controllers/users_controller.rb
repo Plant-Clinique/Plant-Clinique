8951358ccb3abd5cd099b999b97be5525b8ebf6c
Osamah Mandawi <oamandawi@brandeis.edu>
2021-03-14 18:54:41 -0400

    Fixed form to include username and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,13 +58,14 @@ class UsersController < Clearance::UsersController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
