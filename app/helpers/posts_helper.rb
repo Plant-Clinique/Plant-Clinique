1451faedfe5efa2cd13245ce4fa702e428d8ef4e
Osamah Mandawi <oamandawi@brandeis.edu>
2021-03-26 14:05:35 -0400

    Removed sortable

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,2 @@
 module PostsHelper
-  def sortable(column, title = nil)
-    title ||= column.titleize
-    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
-  end
 end
