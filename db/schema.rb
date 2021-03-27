0015dc88d93f8f0c5825a630e4af3338236994ba
Osamah Mandawi <oamandawi@brandeis.edu>
2021-03-27 17:39:09 -0400

    Added PlantType table and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_212606) do
+ActiveRecord::Schema.define(version: 2021_03_27_213139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2021_03_14_212606) do
     t.datetime "time_sent"
     t.boolean "from_bot"
     t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "plant_types", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
