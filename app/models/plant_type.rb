2987f2ebce7f1023ffacd8ebc3344fa8ab512dff
Osamah Mandawi <oamandawi@brandeis.edu>
2021-03-27 17:39:09 -0400

    Added PlantType table and seeds

---
+++ app/models/plant_type.rb
@@ -0,0 +1,2 @@
+class PlantType < ApplicationRecord
+end
---
+++ db/migrate/20210327213139_create_plant_types.rb
@@ -0,0 +1,9 @@
+class CreatePlantTypes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :plant_types do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
