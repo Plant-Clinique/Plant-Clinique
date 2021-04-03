6f4b2351c20acfeacd8ecb9c3bee05e12c6eccd2
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-03 15:01:04 -0400

    RailsAdmin everywhere added :/

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_213139) do
+ActiveRecord::Schema.define(version: 2021_04_03_181429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_03_27_213139) do
     t.string "encrypted_password", limit: 128
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
     t.index ["username"], name: "index_users_on_username", unique: true
