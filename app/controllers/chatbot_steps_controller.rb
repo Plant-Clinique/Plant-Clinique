f78c680f86a53374807f860ac4df83e17d005151
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-05 15:55:51 -0400

    added an exclamation point

--- app/controllers/chatbot_steps_controller.rb
+++ app/controllers/chatbot_steps_controller.rb
@@ -2,7 +2,7 @@ class ChatbotStepsController < ApplicationController
   before_action :require_login
   include Wicked::Wizard
 
-  steps :choose_visit_type, :choose_plant, :select_symptoms, :answer_symptoms_questions, :display_possible_treatments # MORE STEPS GO HERE
+  steps :choose_visit_type, :choose_plant, :select_symptoms, :answer_symptoms_questions, :display_possible_treatments # MORE STEPS GO HERE!
 
   def show
     @user = current_user
