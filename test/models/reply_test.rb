4a04a343b7add34c350702668e44265a53d7871e
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-06 22:36:14 -0400

    User test works

--- test/models/reply_test.rb
+++ test/models/reply_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ReplyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    User.create!(username: "one", email: "two@one.com", password:"three")
+    Post.create!(user_id: User.first.id, title: "hello", body: "world")
+    @reply= Reply.new(user_id: User.first.id, post_id: Post.first.id, body: "hiii"))
+  end
+  test "should be valid" do
+    assert @reply.valid?
+  end
+  test "body should be present" do
+    @reply.body = ""
+    assert_not @reply.valid?
+  end
 end
