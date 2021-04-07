b06d56ebd367a80eec3663a9d6c578acfbaa3b02
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-06 22:48:50 -0400

    Post tests work

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,23 +1,27 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  def setup
+def setup
     User.create!(username: "one", email: "two@one.com", password:"three")
-    @user_plant= UserPlant.new(name: "Example Plant", user_id: User.first.id, age: 1)
+    @post= Post.new(user_id: User.first.id, title: "hello", body: "world")
   end
   test "should be valid" do
-    assert @user_plant.valid?
+    assert @post.valid?
   end
-  test "name should be present" do
-    @user_plant.name = "     "
-    assert_not @user_plant.valid?
+  test "title should be present" do
+    @post.title = "     "
+    assert_not @post.valid?
   end
-  test "age should be present" do
-    @user_plant.age = "     "
-    assert_not @user_plant.valid?
+  test "body should be present" do
+    @post.body = "     "
+    assert_not @post.valid?
   end
-  test "name should not be longer than 100 characters" do
-    @user_plant.name = "a" * 105
-    assert_not @user_plant.valid?
+  test "title should not be longer than 500 characters" do
+    @post.title = "a" * 501
+    assert_not @post.valid?
+  end
+  test "body should not be longer than 3500 characters" do
+    @post.body= "a" * 3501
+    assert_not @post.valid?
   end
 end
