1f6bc61faba22906acce0fc1bf5b345374940149
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-06 22:37:11 -0400

    Reply tests work

--- test/models/reply_test.rb
+++ test/models/reply_test.rb
@@ -4,7 +4,7 @@ class ReplyTest < ActiveSupport::TestCase
   def setup
     User.create!(username: "one", email: "two@one.com", password:"three")
     Post.create!(user_id: User.first.id, title: "hello", body: "world")
-    @reply= Reply.new(user_id: User.first.id, post_id: Post.first.id, body: "hiii"))
+    @reply= Reply.new(user_id: User.first.id, post_id: Post.first.id, body: "hiii")
   end
   test "should be valid" do
     assert @reply.valid?
