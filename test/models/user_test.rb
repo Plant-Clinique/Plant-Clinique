84c5f6c17349112cd5c3902c9a9af43a434a0bd1
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-06 22:24:16 -0400

    fix test action

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    User.create!(username: "one", email: "two@one.com", password:"three")
+    @user_plant= UserPlant.new(name: "Example Plant", user_id: User.first.id, age: 1)
+  end
+  test "should be valid" do
+    assert @user_plant.valid?
+  end
+  test "name should be present" do
+    @user_plant.name = "     "
+    assert_not @user_plant.valid?
+  end
+  test "age should be present" do
+    @user_plant.age = "     "
+    assert_not @user_plant.valid?
+  end
+  test "name should not be longer than 100 characters" do
+    @user_plant.name = "a" * 105
+    assert_not @user_plant.valid?
+  end
 end
