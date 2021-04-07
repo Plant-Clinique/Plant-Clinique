acede648c8bdc10e2b82808118ad015688cd93ca
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-06 22:44:06 -0400

    Reminder tests work

--- test/models/reminder_test.rb
+++ test/models/reminder_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ReminderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    User.create!(username: "one", email: "two@one.com", password:"three")
+    UserPlant.create!(name: "Example Plant", user_id: User.first.id, age: 1)
+    @reminder= Reminder.new(user_plant_id: UserPlant.first.id, user_id: User.first.id, reminder_time: Time.now)
+  end
+  test "should be valid" do
+    assert @reminder.valid?
+  end
+  test "time should be present" do
+    @reminder.reminder_time = ""
+    assert_not @reminder.valid?
+  end
+  test "description doesn't have to be present" do
+    assert @reminder.valid?
+  end
 end
