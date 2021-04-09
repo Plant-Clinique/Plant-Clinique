33bbd464f080ba3b60c9f392328bb2875d84992c
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-09 17:54:30 -0400

    Working beginning steps

--- app/controllers/chatbot_steps_controller.rb
+++ app/controllers/chatbot_steps_controller.rb
@@ -6,25 +6,31 @@ class ChatbotStepsController < ApplicationController
 
   def show
     @user = current_user
-    
+    @plant = "none"
     case step
     when :choose_visit_type
-      @bot_message = ChatbotMessage.create(user_id: current_user.id,
-                                          time_sent: Time.now.utc,
-                                          from_bot: true,
-                                          content: "hello user!")
-      @chatbot_message = ChatbotMessage.new
-      @text = "Yo yo yo choose visit type"
+      # @bot_message = ChatbotMessage.create(user_id: current_user.id,
+      #                                     time_sent: Time.now.utc,
+      #                                     from_bot: true,
+      #                                     content: "hello user!")
+      # @chatbot_message = ChatbotMessage.new
     when :choose_plant
-      @text = "Nice, time to choose a plant"
+      @user_plants = UserPlant.where(user_id: current_user.id)
+    when :select_symptoms
     end
     render_wizard
   end
 
   def update
     @user = current_user
-    @user.attributes = params[:user]
-    render_wizard @user
+    @user_plants = UserPlant.where(user_id: current_user.id)
+    case step
+    when :choose_plant
+      @plant =UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first.name
+    when :select_symptoms
+      @plant =UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first.name
+    end
+    render_wizard
   end
 
   def new
