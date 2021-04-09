cbd0cd57781c3c80451c2cda8ef63517c674a2d5
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-09 18:03:41 -0400

    Fixed edit reply path

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,9 @@ Rails.application.routes.draw do
 
   resources :posts
   resources :posts do
-    resources :replies, only: [:index, :create, :edit]
+    resources :replies, only: [:index, :create]
   end
+  resources :replies, only: [:edit, :show]
   resources :chatbot_messages, except: [:edit, :update, :destroy]
   resources :reminders
   resources :user_plants, except: [:index]
