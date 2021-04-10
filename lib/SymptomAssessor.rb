bfa81b56087a3b6c847362b97c1bc94d01c2de9f
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-10 17:36:43 -0400

    Symptom logic

--- lib/SymptomAssessor.rb
+++ lib/SymptomAssessor.rb
@@ -7,12 +7,20 @@ class SymptomAssessor
     @plant_type = plant_type
     @symptoms = JSON.parse("symptoms.json")["symptoms"]
     # @symptoms = JSON.parse(Net::HTTP.get(URI.parse('https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=6j4O7U0FSKM_Te6mN3aFN7TORBk0RYtU_wk5sJgkjbw')))
-    plant_types = result['data'].map{ |plant| plant['common_name'] }.reject{ |plant| plant == nil }
+    # plant_types = result['data'].map{ |plant| plant['common_name'] }.reject{ |plant| plant == nil }
   end
 
-  def all_symptoms
-    @symptoms[]
+  def all_symptoms_descriptions
+    @symptoms.map(&:description)
   end
 
+  def get_symptom_causes(symptom_id)
+    @symptoms[symptom_id]["causes"].sort_by { |cause| -cause["likelihood"] }
+  end
+
+  def get_cause_question(symptom_id, cause_id)
+    field = @symptoms[symptom_id][cause_id]["check"]
+    check_type = @symptoms[symptom_id][cause_id]["check"]
+  end
 
 end
