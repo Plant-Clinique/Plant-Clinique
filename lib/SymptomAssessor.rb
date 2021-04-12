da4d009f9dc0e388f3727d654d53ede2b96a92e7
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-12 14:23:53 -0400

    Added more fields to the get_diagnosis method

--- lib/SymptomAssessor.rb
+++ lib/SymptomAssessor.rb
@@ -3,6 +3,11 @@ require 'json'
 class SymptomAssessor
   attr_accessor :plant_type, :symptoms
 
+  CHECK_TYPES = { 
+                  "more" : lambda {|ideal, estimate| estimate > ideal},
+                  "less" : lambda {|ideal, estimate| estimate < ideal}
+                }
+
   def initialize(plant_type)
     @plant_type = plant_type
     @symptoms = JSON.parse("symptoms.json")["symptoms"]
@@ -18,9 +23,18 @@ class SymptomAssessor
     @symptoms[symptom_id]["causes"].sort_by { |cause| -cause["likelihood"] }
   end
 
-  def get_cause_question(symptom_id, cause_id)
+  def get_diagnosis(plant_type, symptom_id, cause_id)
     field = @symptoms[symptom_id][cause_id]["check"]
-    check_type = @symptoms[symptom_id][cause_id]["check"]
+    diagnosis = @symptoms[symptom_id][cause_id]["description"]
+    check_type = @symptoms[symptom_id][cause_id]["check_type"]
+    api_ideal_for_plant_type = api + field
+    result = JSON.parse(Net::HTTP.get(URI.parse("https://trefle.io/api/v1/plants/search?token=6j4O7U0FSKM_Te6mN3aFN7TORBk0RYtU_wk5sJgkjbw&q=#{plant.plant_type.parameterize.underscore}")))
+    "#{plant.name} is a #{plant.plant_type} and its scientific name is #{result["data"][0]["scientific_name"].titleize}"
+    if check_type:
+      if CHECK_TYPES[check_type](api_ideal_for_plant_type, user_plant_estimate)
+        return diagnosis
+      end
+    end
   end
 
 end
