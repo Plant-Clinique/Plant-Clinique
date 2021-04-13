782fd5c7212a3f91135c6f2fc5e977c1bedafb57
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-12 22:03:32 -0400

    Fixed chatbot logic and fixed environment reload and made it so that chatbot gets next questions

--- app/controllers/chatbot_steps_controller.rb
+++ app/controllers/chatbot_steps_controller.rb
@@ -1,12 +1,25 @@
+require './lib/symptom_accessor'
+
 class ChatbotStepsController < ApplicationController
   before_action :require_login
+  before_action :set_shared_vars
+
   include Wicked::Wizard
 
   steps :choose_visit_type, :choose_plant, :select_symptoms, :answer_symptoms_questions, :display_possible_treatments # MORE STEPS GO HERE!
-
-  def show
+    
+  def set_shared_vars
     @user = current_user
-    @plant = "none"
+    @plant 
+    @symptoms 
+    @question 
+    @symptom_assessor
+    @symptom_causes
+    @user_plants = UserPlant.where(user_id: current_user.id)
+    @possible_treatments = []
+  end
+  
+  def show
     case step
     when :choose_visit_type
       # @bot_message = ChatbotMessage.create(user_id: current_user.id,
@@ -15,22 +28,49 @@ class ChatbotStepsController < ApplicationController
       #                                     content: "hello user!")
       # @chatbot_message = ChatbotMessage.new
     when :choose_plant
-      @user_plants = UserPlant.where(user_id: current_user.id)
+      @plant = UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first
     when :select_symptoms
-      @symptom_assessment = SymptomAssessment.new()
-      @question
+      @symptoms = @symptom_assessor.get_symptom_select_options
+    when :answer_symptoms_questions
+      @question = @symptom_assessor.get_question_using_cause(@symptom_causes[0])
     end
+
     render_wizard
+
   end
 
   def update
-    @user = current_user
-    @user_plants = UserPlant.where(user_id: current_user.id)
+    @question_number = 0
     case step
     when :choose_plant
-      @plant =UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first.name
+      @plant = UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first
     when :select_symptoms
-      @plant =UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first.name
+      @plant = UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first
+      @symptom_assessor = SymptomAssessor.new(@plant.plant_type)
+      @symptoms = @symptom_assessor.get_symptom_select_options
+    when :answer_symptoms_questions
+      @plant = UserPlant.where(user_id: current_user.id, id: params[:plant_id].to_i).first
+      @symptom_assessor = SymptomAssessor.new(@plant.plant_type)
+      @symptom_id = params[:symptom_id].to_i
+      @symptom_causes = @symptom_assessor.get_symptom_causes(@symptom_id)
+
+      if params[:question_number]
+        @question_number = params[:question_number].to_i 
+      else
+        @question_number = 0
+      end
+
+      if params[:symptom_question_response]
+        diagnosis = @symptom_assessor.get_diagnosis(@plant.plant_type,@symptom_id,@question_number-1, params[:symptom_question_response].to_i)
+        @possible_treatments.append()
+      end
+
+      if @question_number <= @symptom_causes.length
+        @question = @symptom_assessor.get_question_using_cause(@symptom_causes[@question_number])
+        @question_number += 1
+      else
+        jump_to(:display_possible_treatments)
+      end
     end
     render_wizard
   end
@@ -38,4 +78,5 @@ class ChatbotStepsController < ApplicationController
   def new
     @chatbot_message = ChatbotMessage.new
   end
+
 end
---
+++ lib/symptom_accessor.rb
@@ -0,0 +1,88 @@
+require 'json'
+require 'net/http'
+
+class SymptomAssessor
+  attr_accessor :plant_type, :symptoms
+
+  CHECK_TYPE = { 
+                  "more" => lambda {|ideal, estimate| estimate > ideal},
+                  "less" => lambda {|ideal, estimate| estimate < ideal}
+                }
+  
+  CHECK_TARGET = {
+                  "soil_humidity" => lambda {
+                    |plant_data| plant_data['growth']['soil_humidity']
+                  },
+                  "maximum_temperature" => lambda {
+                    |plant_data| plant_data['growth']['maximum_temperature']['deg_f']
+                  },
+                  "minimum_temperature" => lambda {
+                    |plant_data| plant_data['growth']['minimum_temperature']['deg_f']
+                  },
+                  "minimum_root_depth" => lambda {
+                    |plant_data| plant_data['growth']['minimum_root_depth']['cm']
+                  },
+                  "soil_nutriments" => lambda {
+                    |plant_data| plant_data['growth']['soil_nutriments']
+                  },
+                  "light" => lambda {
+                    |plant_data| plant_data['growth']['light']
+                  }
+                }
+
+  CHECK_QUESTION = {
+                  "soil_humidity" => "On a scale of 0 to 10, how moist is your plant's soil usually?",
+                  "maximum_temperature" => "What is the warmest temperature, in fahrenheit, reached every day where your plant is?",
+                  "minimum_temperature" => "What is the coldest temperature, in fahrenheit, reached every day where your plant is?",
+                  "minimum_root_depth" =>  "How many centimeters deep did you bury your plant's seeds?",
+                  "soil_nutriments" => "On a scale of 0 to 10, how much fertilizer is in your plant's soil?",
+                  "light" => "On a scale of 0 to 10, how much sunlight does your plant get every day?"
+                }
+  
+  FILE_PATH = File.join(Rails.root, 'lib', 'assets', 'symptoms.json')
+
+  def initialize(plant_type)
+    @plant_type = plant_type
+    file = File.read(FILE_PATH)
+    @symptoms = JSON.parse(file)["symptoms"]
+  end
+
+  def get_symptom_select_options
+    @symptoms.map {|symptom| [symptom['description'], symptom['id']]}
+  end
+
+  def get_symptom_causes(symptom_id)
+    @symptoms[symptom_id]['causes'].sort_by { |cause| -cause['likelihood'] }
+  end
+
+  def get_question_using_id(symptom_id, cause_id)
+    cause_info = get_symptom_causes(symptom_id)[cause_id]
+    CHECK_QUESTION[cause_info['check']]
+  end
+
+  def get_question_using_cause(cause_info)
+    CHECK_QUESTION[cause_info['check']]
+  end
+
+  def get_diagnosis(plant_trefle_id, symptom_id, cause_id, user_plant_estimate)
+    cause_info = get_symptom_causes(symptom_id)[cause_id]
+    field = cause_info['check']
+    diagnosis = cause_info['description']
+    check_type = cause_info['check_type']
+
+    plant_data = JSON.parse(Net::HTTP.get(
+                                URI.parse(
+                                  "https://trefle.io/api/v1/plants/#{plant_trefle_id}?token=6j4O7U0FSKM_Te6mN3aFN7TORBk0RYtU_wk5sJgkjbw"
+                                )
+                              )
+                            )['data']['main_species']
+
+    api_ideal_for_plant_type = CHECK_TARGET[field].call(plant_data)
+    if check_type
+      if CHECK_TYPE[check_type].call(api_ideal_for_plant_type, user_plant_estimate)
+        return diagnosis
+      end
+    end
+  end
+
+end
