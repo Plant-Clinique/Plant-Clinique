50c14c522afe596328c1bdf25d4db7b3c8b286b3
Osamah Mandawi <oamandawi@brandeis.edu>
2021-04-30 21:25:01 -0400

    Cleaned up reminders controller

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -8,7 +8,7 @@ class RemindersController < ApplicationController
   def index
     @current_category = params[:reminder_type] || "All"
     @reminders = RemindersUtils.reminders_by_category(current_user, params[:reminder_type])
-    @reminder_dropdown_list = reminder_type_dropdown.map{ |reminder_type| reminder_type[0].titleize }
+    @reminder_dropdown_list = RemindersUtils.reminder_type_dropdown.map{ |reminder_type| reminder_type[0].titleize }
   end
 
   # GET /reminders/1 or /reminders/1.json
@@ -18,14 +18,14 @@ class RemindersController < ApplicationController
   # GET /reminders/new
   def new
     @reminder = Reminder.new
-    @reminder_dropdown_list = reminder_type_dropdown
-    @user_plant_dropdown = user_plant_dropdown
+    @reminder_dropdown_list = RemindersUtils.reminder_type_dropdown
+    @user_plant_dropdown = RemindersUtils.user_plant_dropdown(current_user)
   end
 
   # GET /reminders/1/edit
   def edit
-    @reminder_dropdown_list = reminder_type_dropdown
-    @user_plant_dropdown = user_plant_dropdown
+    @reminder_dropdown_list = RemindersUtils.reminder_type_dropdown
+    @user_plant_dropdown = RemindersUtils.user_plant_dropdown(current_user)
   end
 
   # POST /reminders or /reminders.json
@@ -74,15 +74,6 @@ class RemindersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def reminder_params
       params.require(:reminder).permit(:user_plant_id, :user_id, :tick_time, :description, :reminder_type, :interval)
-    end
-
-    def reminder_type_dropdown
-      return Reminder.reminder_enums
-    end
-
-    def user_plant_dropdown
-      this_user_plants = UserPlant.where(user_id: current_user)
-      return this_user_plants.pluck(:name).zip(this_user_plants.pluck(:id))
     end
 
 end
--- lib/reminders_utils.rb
+++ lib/reminders_utils.rb
@@ -6,4 +6,13 @@ class RemindersUtils
       Reminder.where(user_id: user) 
     end
   end
+
+  def self.reminder_type_dropdown
+    return Reminder.reminder_enums
+  end
+
+  def self.user_plant_dropdown(user)
+    this_user_plants = UserPlant.where(user_id: user)
+    return this_user_plants.pluck(:name).zip(this_user_plants.pluck(:id))
+  end
 end
