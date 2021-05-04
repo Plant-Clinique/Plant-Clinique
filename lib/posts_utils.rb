f812dd348d7f619bc0d727b5818c1dd250a329fa
Osamah Mandawi <oamandawi@brandeis.edu>
2021-05-03 23:38:09 -0400

    Implemented related posts on user page

--- lib/posts_utils.rb
+++ lib/posts_utils.rb
@@ -11,4 +11,16 @@ class PostsUtils
       list.ordered_by_newest
     end
   end
+  def self.get_related_posts(plants)
+    related_posts = [] # an array of related posts 
+    plants.each do |plant|
+      plant_type = PlantType.where(trefle_id: plant.plant_type).first.name
+      Post.all.each do |post|
+        if post.body.downcase.include?(plant_type.downcase) or post.title.downcase.include?(plant_type.downcase)
+          related_posts << post
+        end
+      end
+    end
+    return related_posts
+  end
 end
