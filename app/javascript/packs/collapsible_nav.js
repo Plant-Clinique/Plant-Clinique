4212b010a0f9047a70d13651de0e91df2420e41b
Osamah Mandawi <oamandawi@brandeis.edu>
2021-05-09 16:18:30 -0400

    Refactor collapsible navbar script

---
+++ app/javascript/packs/collapsible_nav.js
@@ -0,0 +1,21 @@
+$(window).resize(function() {
+  if ($(this).width() < 1300) {
+    $('.uncollapsed').hide(); /* hide uncollapsed navbar version */ 
+    $('.collapsed-nav').show(); /* show collapsed */
+  } else {
+    $('.collapsed-nav').hide(); /* hide collapsed */ 
+    $('.uncollapsed').show(); /* show uncollapsed */
+  }
+});
+
+$(document).ready(function() {
+  if ($(this).width() < 1300) {
+    $('.uncollapsed').hide(); /* hide uncollapsed navbar version */ 
+    $('.collapsed-nav').show(); /* show collapsed */
+  }
+  else  {
+    $('.collapsed-nav').hide(); /* hide collapsed */ 
+    $('.uncollapsed').show(); /* show uncollapsed */
+  }
+  console.log('welcome to the plant-clinique log')
+});
