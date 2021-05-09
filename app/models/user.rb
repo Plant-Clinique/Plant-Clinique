e7e441297878031b7c3ed10a00a3fbb2704c825f
RomanEmpire314 <taromka21@brandeis.edu>
2021-05-09 17:11:49 -0400

    Add user full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   include Gravtastic
-  gravtastic
-  
+  gravtastic 
   include Clearance::User
 
     has_many :user_plants
@@ -10,5 +9,7 @@ class User < ApplicationRecord
     has_many :replies
     has_many :posts
     
-    validates :username, presence: true, uniqueness: true
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true
+
 end
